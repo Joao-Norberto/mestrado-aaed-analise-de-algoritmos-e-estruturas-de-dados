6ab7537dde361f59b23ce6d60cc885e8eae1d0b6
João Pedro Norberto da Silva <joao@techforus.com.br>
2025-03-31 18:47:00 -0300

    add iteractive function

--- joao_pedro_norberto_da_silva_aula1Pratico.py
+++ joao_pedro_norberto_da_silva_aula1Pratico.py
@@ -1,14 +1,18 @@
 import random
 
-random_vector = [random.randint(1, 1000000) for i in range(100000)]
+random_vector = [random.randint(1, 1000000) for _ in range(100000)]
 
-def is_prime_number(n: int):
+def is_prime_number(n: int) -> bool:
     if n <= 1:
         return False
-    
-    for i in range(2, n//2):
-        if n % i == 0:# se o resto da divisão for 0, a divisão é exata e o número não é primo
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
             return False
-
     return True
+
+result_list = [is_prime_number(n) for n in random_vector]
 
