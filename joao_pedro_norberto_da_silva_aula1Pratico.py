52235717deb7068cde720f1dc8c3348754dc0173
João Pedro Norberto da Silva <joao@techforus.com.br>
2025-03-31 18:25:59 -0300

    add is prime function

--- joao_pedro_norberto_da_silva_aula1Pratico.py
+++ joao_pedro_norberto_da_silva_aula1Pratico.py
@@ -2,3 +2,13 @@ import random
 
 random_vector = [random.randint(1, 1000000) for i in range(100000)]
 
+def is_prime_number(n: int):
+    if n <= 1:
+        return False
+    
+    for i in range(2, n//2):
+        if n % i == 0:# se o resto da divisão for 0, a divisão é exata e o número não é primo
+            return False
+
+    return True
+
