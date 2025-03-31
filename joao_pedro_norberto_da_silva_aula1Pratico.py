0c970e8eee16cd0eb41c919ed201a1389618cd74
João Pedro Norberto da Silva <joao@techforus.com.br>
2025-03-31 18:52:43 -0300

    add recursive function

--- joao_pedro_norberto_da_silva_aula1Pratico.py
+++ joao_pedro_norberto_da_silva_aula1Pratico.py
@@ -1,6 +1,19 @@
 import random
 
-random_vector = [random.randint(1, 1000000) for _ in range(100000)]
+random_vector = [random.randint(1, 1000000) for i in range(100000)]
+
+def is_prime_recursive(n: int, divisor: int = 2) -> bool:
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % divisor == 0:
+        return False
+    if divisor * divisor > n:
+        return True
+
+    return is_prime_recursive(n, divisor + 1 if divisor == 2 else divisor + 2)
+
 
 def is_prime_number(n: int) -> bool:
     if n <= 1:
@@ -14,5 +27,6 @@ def is_prime_number(n: int) -> bool:
             return False
     return True
 
-result_list = [is_prime_number(n) for n in random_vector]
+result_list = [is_prime_recursive(n) for n in random_vector]
+print(result_list[:10])
 
